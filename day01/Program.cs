2bc604a28b324c64b2ddac75be75371a130b9eeb
Jokinko <jozefchmelar@users.noreply.github.com>
2021-12-01 15:14:55 +0100

    aoc day 1 in c#

---
+++ day01/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+var values = File.ReadAllLines("input.txt").Select(x =>Int32.Parse(x));
+
+var calc = values
+    .Zip(values.Skip(1), (first,second) => first < second ? 1:0)
+    .Sum();
+
+WriteLine(calc);
+
+var sumOfWindowsOf3 = values
+    .Zip(values.Skip(1).Zip(values.Skip(2)))
+    .Select( (a,b) => a.First + a.Second.First + a.Second.Second );
+var calc2 = sumOfWindowsOf3
+    .Zip(sumOfWindowsOf3.Skip(1), (first,second) => first < second ? 1:0)
+    .Sum();
+
+WriteLine(calc2);
